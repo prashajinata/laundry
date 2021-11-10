24daa903c41b6bbddbda02523ecc20ee114163f7
ajinata84 <ajinata89@gmail.com>
2021-11-10 08:58:30 +0700

    initial comit

---
+++ routers/paket.js
@@ -0,0 +1,77 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+const models = require("../models/index")
+
+const paket = models.paket
+
+app.get("/", async (request, response) => {
+    let dataPaket = await paket.findAll()
+    return response.json(dataPaket)
+})
+
+app.post("/", (request, response) => {
+    let newPaket = {
+        jenis_paket: request.body.jenis_paket,
+        harga: request.body.harga
+    }
+
+    paket.create(newPaket)
+        .then(result => {
+            response.json({
+                message: "Data Berhasil Dibuat",
+                data:result
+            })
+        })
+        .catch(error => {
+            response.json({
+                message: error.message
+            })
+        })
+})
+
+app.put("/:id_paket", (request, response) => {
+    let data = {
+        jenis_paket: request.body.jenis_paket,
+        harga: request.body.harga
+    }
+
+    let parameter = {
+        id_paket: request.params.id_paket
+    }
+
+    paket.update(data, { where: parameter })
+        .then(result => {
+            return response.json({
+                message: "data sukses diubah",
+                data: data
+            })
+        })
+        .catch(error => {
+            return response.json({
+                message: error.message
+            })
+        })
+})
+
+app.delete("/:id_paket", (request, response) => {
+    let parameter = {
+        id_paket: request.params.id_paket
+    }
+
+    paket.destroy({ where: parameter })
+        .then(result => {
+            return response.json({
+                message: "Data sukses dihapus"
+            })
+        })
+        .catch(error => {
+            return response.json({
+                message: error.message
+            })
+        })
+})
+
+module.exports = app
---
+++ routers/users.js
@@ -0,0 +1,80 @@
+const express = require("express")
+const app = express()
+const md5 = require("md5")
+app.use(express.json())
+
+const models = require("../models/index")
+const users = models.users
+
+app.get("/", async (request, response) => {
+    let dataUser = await users.findAll()
+    return response.json(dataUser)
+})
+
+app.post('/', (request, response) => {
+    let newUsers = {
+        nama: request.body.nama,
+        username: request.body.username,
+        password: md5(request.body.password),
+        role: request.body.role
+    }
+    users.create(newUsers)
+        .then(result => {
+            return response.json({
+                message: "Data berhasil ditambahkan",
+                data: result,
+            })
+        })
+        .catch(error => {
+            return response.json({
+                message: error.message
+            })
+        })
+})
+
+app.put("/:id_user", (request, response) => {
+    let data = {
+        nama: request.body.nama,
+        username: request.body.username,
+        role: request.body.role
+    }
+    if (request.body.password) {
+        data.password = md5(request.body.password)
+    }
+    let parameter = {
+        id_user: request.params.id_user
+    }
+    users.update(data, { where: parameter })
+        .then(result => {
+            return response.json({
+                message: "data sukses diubah",
+                data: data,
+                kondisi: result
+            })
+        })
+        .catch(error => {
+            return response.json({
+                message: error.message
+            })
+        })
+})
+
+app.delete("/:id_user", (request, response) => {
+    let parameter = {
+        id_user: request.params.id_user
+    }
+
+    users.destroy({ where: parameter })
+        .then(result => {
+            return response.json({
+                message: "data sukses dihapus"
+            })
+        })
+        .catch(error => {
+            return response.json({
+                message: error.message
+            })
+        })
+})
+
+module.exports = app
